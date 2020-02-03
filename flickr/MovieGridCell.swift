98a43b373035b1be7d3c184fa325ce9442840c79
Siddharth Dhar <dhar.siddharth1@gmail.com>
2020-02-02 20:33:02 -0500

    commit-p2

---
+++ flickr/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flickr
+//
+//  Created by Siddharth Dhar on 1/31/20.
+//  Copyright © 2020 Siddharth Dhar. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
